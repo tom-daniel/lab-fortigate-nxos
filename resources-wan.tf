e0454bb3479c42b8da3602bb481697cd07c28942
Tom Daniel <tdaniel@fournet.co.uk>
2024-12-23 12:02:48 +0000

    add bgp

--- resources-wan.tf
+++ resources-wan.tf
@@ -1,4 +1,6 @@
 resource "nxos_rest" "vlan-2000" {
+  for_each     = toset([for torswitch in local.torswitches : torswitch.name])
+  device       = each.key
   dn         = "sys/bd/bd-[vlan-2000]"
   class_name = "l2BD"
   content = {
@@ -31,15 +33,58 @@ resource "nxos_physical_interface" "wan-uplink-1" {
 }
 
 
-resource "fortios_system_interface" "interface-vlan-2000" {
+resource "fortios_system_interface" "interface-abc-a" {
   defaultgw     = "enable"
   ip            = "10.200.0.2 255.255.255.252"
   mode          = "static"
-  name          = "vlan2000"
+  name          = "abc-a"
   interface     = "port5"
   vlan_protocol = "8021q"
   vlanid        = 2000
   vdom          = "root"
   description   = "Created by Terraform Provider for FortiOS"
   allowaccess   = "ping"
+}
+
+resource "fortios_system_interface" "interface-abc-b" {
+  defaultgw     = "enable"
+  ip            = "10.200.0.130 255.255.255.252"
+  mode          = "static"
+  name          = "abc-b"
+  interface     = "port6"
+  vlan_protocol = "8021q"
+  vlanid        = 2000
+  vdom          = "root"
+  description   = "Created by Terraform Provider for FortiOS"
+  allowaccess   = "ping"
+}
+
+
+resource "fortios_router_bgp" "fortigate-bgp" {
+  depends_on = [ fortios_system_interface.interface-abc-a, fortios_system_interface.interface-abc-b ]
+  router_id = "10.30.12.3"
+  as = 50
+  neighbor_group {
+    name = "abc-a"
+    activate = "enable"
+    #interface = "abc-a"
+    remote_as = 100
+  }
+
+  neighbor_group {
+    name = "abc-b"
+    activate = "enable"
+    #interface = "abc-b"
+    remote_as = 100
+  }
+
+  neighbor_range {
+    prefix = "10.200.0.0/25"
+    neighbor_group = "abc-a"
+  }
+
+  neighbor_range {
+    prefix = "10.200.0.128/25"
+    neighbor_group = "abc-b"
+  }
 }
